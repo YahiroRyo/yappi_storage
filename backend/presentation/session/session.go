31cc1601917a9b17f4073dcda57d0faf7325c6e1
八尋 諒 <yahiroryo@email.yappi.jp>
2025-05-18 18:33:31 +0900

    fix session

--- backend/presentation/session/session.go
+++ backend/presentation/session/session.go
@@ -1,7 +1,6 @@
 package session
 
 import (
-	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,14 +9,26 @@ import (
 	"github.com/gofiber/storage/sqlite3"
 )
 
-var storage = sqlite3.New(sqlite3.ConfigDefault)
-var store = session.New(session.Config{
-	Storage:      storage,
-	Expiration:   24 * time.Hour,
-	KeyLookup:    "cookie:session_id",
-	KeyGenerator: utils.UUIDv4,
-	CookieDomain: os.Getenv("HOSTNAME"),
-})
+var storage *sqlite3.Storage
+var store *session.Store
+
+func init() {
+	storage = sqlite3.New(sqlite3.ConfigDefault)
+	if storage == nil {
+		panic("failed create sqlite3 instance")
+	}
+
+	store = session.New(session.Config{
+		Storage:      storage,
+		Expiration:   24 * time.Hour,
+		KeyLookup:    "cookie:session_id",
+		KeyGenerator: utils.UUIDv4,
+		CookieName:   "session_id",
+	})
+	if store == nil {
+		panic("failed create store instance")
+	}
+}
 
 func GetSession(ctx *fiber.Ctx) (*session.Session, error) {
 	sess, err := store.Get(ctx)
