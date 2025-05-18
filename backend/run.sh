95dc2665254f0825cc1f6a307a618116255895c0
八尋 諒 <yahiroryo@email.yappi.jp>
2025-05-18 16:19:44 +0900

    fix run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main() {
-    cd ~/backend/infrastructure/dactabase/goose || exit
+    cd ~/go/src/github.com/YahiroRyo/yappi_storage/backend/infrastructure/dactabase/goose || exit
     go tool goose up
     cd ~/ || exit
     go build -o backend ./main.go
