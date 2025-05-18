f530fd62900654f80ce235329598b295f5e8eba3
八尋 諒 <yahiroryo@email.yappi.jp>
2025-05-18 16:24:47 +0900

    fix run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 main() {
-    cd /go/src/github.com/YahiroRyo/yappi_storage/backend/infrastructure/database/goose || exit
-    go tool goose up
+    cd /go/src/github.com/YahiroRyo/yappi_storage/backend/infrastructure/database/goose && go tool goose up
     cd /go/src/github.com/YahiroRyo/yappi_storage/backend || exit
     go build -o backend ./main.go
     ./backend
